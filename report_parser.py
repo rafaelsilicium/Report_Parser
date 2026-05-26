28342ed2d87f859da808319cbf28d3595f1fca9d
Rafael R. Schaeffer <rafschaeffer@gmail.com>
2026-05-26 15:08:31 -0300

    Refatora o parser de relatórios para maior modularidade e robustez

--- report_parser.py
+++ report_parser.py
@@ -1,115 +1,120 @@
-import os
 from pathlib import Path
 import csv
 
-caminho_pasta_relatorios = "C:/Users/rafsc/OneDrive/Área de Trabalho/Relatorios txt"
-caminho_arquivo_csv = (
-    "C:/Users/rafsc/OneDrive/Área de Trabalho/Relatorios txt/projetos.csv"
-)
-headers = ["NOME DO PROJETO", "DATA", "STATUS", "RESUMO"]
 
-Path_relatorios = Path(caminho_pasta_relatorios)
-
-
-def atualiza_txt_incompleto(arquivo_relatorio, headers_ausentes):
-
-    with open(arquivo_relatorio, "a", encoding="utf-8-sig") as editar_txt:
-        for header in headers_ausentes:
-            editar_txt.write(f"\n{header}: NÃO INFORMADO")
-
-
-def strip_conteudo_resumo(conteudo_relatorio):
-    return [linha.strip() for linha in conteudo_relatorio]
-
-
-def verifica_headers_ausentes(conteudo_relatorio):
+def verifica_headers_ausentes(conteudo_relatorio, headers):
 
     headers_ausentes = []
     for header in headers:
-        if not (
-            any(
-                header.strip() in linha
-                for linha in strip_conteudo_resumo(conteudo_relatorio)
-            )
-        ):
-            headers_ausentes.append(header)
+        if not (any(header.strip() in linha for linha in conteudo_relatorio)):
+            headers_ausentes.append(header.strip())
 
     return headers_ausentes
 
 
-def organiza_conteudo_txt_em_dicionario(conteudo_relatorio):
+def organiza_conteudo_txt_em_dicionario(conteudo_relatorio, headers_ausentes):
 
     conteudos_separados = {}
     for linha in conteudo_relatorio:
         if ":" in linha:
-            header, conteudo = linha.split(":")
-            conteudos_separados[header.strip()] = linha.strip(header).strip(": ")
+            header, conteudo = linha.split(":", 1)
+            conteudos_separados[header.strip()] = conteudo.strip()
+
+    if len(headers_ausentes) != 0:
+        for header in headers_ausentes:
+            conteudos_separados[header.strip()] = "NÃO INFORMADO"
 
     return conteudos_separados
 
 
-def atualiza_leitura_relatorio(arquivo_relatorio):
+def leitura_relatorio(arquivo_relatorio):
 
     return arquivo_relatorio.read_text(encoding="utf-8-sig").splitlines()
 
 
-def leitura_arquivos_txt():
+def leitura_arquivos_txt(path_relatorios, headers):
 
-    conteudos_agrupados = (
-        []
-    )  # Variável que receberá o conteúdo dos arquivos .txt unidos
+    conteudos_agrupados = []
     print("\nIniciando a leitura dos arquivos .txt\n")
 
-    for arquivo_relatorio in Path_relatorios.glob("*.txt"):
+    for arquivo_relatorio in path_relatorios.glob("*.txt"):
+        try:
+            print(f"Analisando relatório {arquivo_relatorio}")
 
-        print(f"Analizando relatório {arquivo_relatorio}")
+            conteudo_relatorio = leitura_relatorio(arquivo_relatorio)
 
-        conteudo_relatorio = atualiza_leitura_relatorio(arquivo_relatorio)
+            if arquivo_relatorio.stat().st_size != 0:
 
-        if arquivo_relatorio.stat().st_size != 0:
+                headers_ausentes = verifica_headers_ausentes(
+                    conteudo_relatorio, headers
+                )
+                conteudos_agrupados.append(
+                    organiza_conteudo_txt_em_dicionario(
+                        conteudo_relatorio, headers_ausentes
+                    )
+                )
 
-            headers_ausentes = verifica_headers_ausentes(conteudo_relatorio)
-            if len(headers_ausentes) != 0:
-                # O código atualiza os relatorios txt incompletos, criando campos "NÃO INFORMADO" nas informações ausentes.]
-                # Essa decisão foi tomada para facilitar a criação do csv, sendo o método que o dev conseguiu implementar na
-                # versão do código.
-                atualiza_txt_incompleto(arquivo_relatorio, headers_ausentes)
-                conteudo_relatorio = atualiza_leitura_relatorio(arquivo_relatorio)
+        except Exception as erro:
+            print(f"Erro! Um erro inesperado aconteceu! Erro: {erro}")
 
-            conteudos_agrupados.append(
-                organiza_conteudo_txt_em_dicionario(conteudo_relatorio)
-            )
-
-    return conteudos_agrupados  # Retorna a variável com todos os conteúdos unidos
+    return conteudos_agrupados
 
 
-def insere_headers_csv(planilha_csv):
+def insere_headers_csv(planilha_csv, headers):
     dicionar_headers = csv.writer(planilha_csv, delimiter=";")
     dicionar_headers.writerow(headers)
-    print("Cabeçalho do arquivo .csv adicionado!")
+    print("\nCabeçalho do arquivo .csv adicionado!")
 
 
-def cria_arquivo_csv():
+def cria_arquivo_csv(path_arquivo_csv, dados_parser, headers):
 
-    with open(
-        caminho_arquivo_csv, "a", newline="", encoding="utf-8-sig"
-    ) as planilha_csv:
+    try:
+        with open(
+            path_arquivo_csv, "a", newline="", encoding="utf-8-sig"
+        ) as planilha_csv:
 
-        if os.path.getsize(caminho_arquivo_csv) == 0:
-            insere_headers_csv(planilha_csv)
+            if path_arquivo_csv.stat().st_size == 0:
+                insere_headers_csv(planilha_csv, headers)
 
-        editar_csv = csv.DictWriter(planilha_csv, fieldnames=headers, delimiter=";")
-        editar_csv.writerows(leitura_arquivos_txt())
-        planilha_csv.close()
-        print("\nEdição do arquivo .csv finalizada!")
+            editar_csv = csv.DictWriter(planilha_csv, fieldnames=headers, delimiter=";")
+            editar_csv.writerows(dados_parser)
+            print("\nEdição do arquivo .csv finalizada!")
+
+    except FileNotFoundError:
+        print("Erro! O arquivo .csv indicado não existe!")
+
+    except Exception as erro:
+        print(
+            f"\nErro! Não foi possível abrir o arquivo .csv! \nA edição não foi realizada! Erro: {erro}"
+        )
 
 
 def main():
 
-    print("\n--------Report Parser V1--------\n")
-    cria_arquivo_csv()
+    path_arquivo_csv = Path(
+        "C:/Users/rafsc/OneDrive/Área de Trabalho/Relatorios txt/projetos.csv"
+    )
+    path_relatorios = Path("C:/Users/rafsc/OneDrive/Área de Trabalho/Relatorios txt")
+    headers = ["NOME DO PROJETO", "DATA", "STATUS", "RESUMO"]
 
-    print("Fim da execução...")
+    print("\n--------Report Parser V2--------\n")
+
+    if path_relatorios.exists() and path_relatorios.is_dir():
+
+        dados_parser = leitura_arquivos_txt(path_relatorios, headers)
+
+        if len(dados_parser) != 0:
+            cria_arquivo_csv(path_arquivo_csv, dados_parser, headers)
+
+        else:
+            print(
+                "Atenção! A leitura dos relatórios retornou nenhum dado. Execução não realizada."
+            )
+
+    else:
+        print("Erro! O diretório indicado para os relatórios não existe!")
+
+    print("\nFim da execução...")
 
 
 if __name__ == "__main__":
